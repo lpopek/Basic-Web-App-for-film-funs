3cc4218a92c59472700b56a2bb9152e7dece1dea
Łukasz Popek <01123885@pw.edu.pl>
2020-11-17 16:30:40 +0100

    10

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("menuBar");
-    if (x.className === "menuBar") {
-      x.className += " responsive";
-    } else {
-      x.className = "menuBar";
-    }
-  }
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+document.querySelector('.formsStyle button').onclick = function() {
+    alert('Och niee jeszcze ta opcja niedstępa musisz poczekać chwilę:((!');
+}
+document.querySelector('.formsStyle button').onclick = function() {
+  alert('Och niee jeszcze ta opcja niedstępa musisz poczekać chwilę:((!');
 }
