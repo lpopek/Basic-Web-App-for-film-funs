385c30679e26aea8dc34393e03a844e7bc0cb68a
Łukasz Popek <01123885@pw.edu.pl>
2020-12-06 15:18:55 +0100

    save data to file from form

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 var registrButton = document.querySelectorAll('#formsStyle button')[0];
 var resetButton = document.querySelectorAll('#formsStyle button')[1];
 console.log(document.getElementById("#formsStyle"));
-resetButton.addEventListener('click', reset);
-
-// var formsButtons = Array.prototype.filter.call(formsElements[0].childNodes, function(buttonElements){
-//   return buttonElements.nodeName === 'BUTTON';
-// });
-
-var textInput = document.querySelectorAll('#formsStyle input[type="text"]');
-for(var i=0; i < textInput.length; i++){
-  textInput[i].addEventListener('keyup', writing)
-}
-var emailInput = document.querySelector('#formsStyle input[type="email"]');
-emailInput.addEventListener('keyup', writing)
-var ageInput = document.querySelector('#formsStyle input[type="number"]');
-ageInput.addEventListener('change', writing)
-var datetInput = document.querySelector('#formsStyle input[type="date"]');
-datetInput.addEventListener('change', writing)
-var genderInput =  document.querySelector('#formsStyle select');
-datetInput.addEventListener('change', writing)
 
 
-function reset(){
-  document.getElementById("formsStyle").childNodes.reset();
+function resetForm(){
+  document.getElementById("formsStyle").reset();
 }
 
-function writing(element){
-  var string = element.target.value;
-  console.log(string);
-}
 
+function saveToTxt() {
+    	
+  // Get the data from each element on the form.
+  const name = document.getElementById('txtName');
+  const surname = document.getElementById('txtSurname');
+  const birtday = document.getElementById('txtBirthday');
+  const email = document.getElementById('txtEmail');
+  const gender = document.getElementById('txtGender');
+  
+  let data = 
+      '\r Name: ' + name.value + ' \r\n ' + 
+      'Surname: ' + surname.value + ' \r\n ' + 
+      'Email: ' + email.value + ' \r\n ' + 
+      'date of birthday: ' + birtday.value + ' \r\n ' + 
+      'Gender: ' + gender.value;
+  
+  const textToBLOB = new Blob([data], { type: 'text/plain' });
+  const sFileName = 'formData.txt';	   // The file to save the data.
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+      newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+      newLink.href = window.URL.createObjectURL(textToBLOB);
+      newLink.style.display = "none";
+      document.body.appendChild(newLink);
+  }
+
+  newLink.click(); 
+}
