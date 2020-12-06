39522c07099f412b89b02c839e73d7b29126018a
okrupa <o.krupa@stud.elka.pw.edu.pl>
2020-12-06 21:44:11 +0100

    Add search by genre and premiers

---
+++ search_genre.js
@@ -0,0 +1,95 @@
+const movieContainer = document.getElementById('movie-ctr');
+
+const optionsGen = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/genre/movie/list?api_key=b9e8ab5199d674481be8e14eb992dc6a&language=en-US',
+};
+
+function getByGenre(genre){
+    const options = {
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=b9e8ab5199d674481be8e14eb992dc6a&with_genres=${genre}`,
+        }; 
+    return axios.request(options).then(response => response.data.results)
+}
+
+function getGenres(){
+    return axios.request(optionsGen).then(response => response.data.genres)
+}
+
+function getGenresInMovie(movie, genres){
+    movieGenres = String();
+    var str = " ";
+    mGenres = Array();
+    mGenres = movie.genre_ids
+    mGenres.forEach(genre => {
+
+        genres.forEach(gen=> {
+        var g = gen.id;
+            if (genre == g){
+                movieGenres= movieGenres + str + gen.name;
+            };
+        });
+        
+    }
+    );
+    return movieGenres
+}
+
+function clearDiv(){
+    while (movieContainer.firstChild) {
+        movieContainer.removeChild(movieContainer.firstChild);
+      }
+}
+
+async function searchByGenre(genre) {
+    movies = Array();
+    genres = Array();
+    clearDiv();
+    var promise = getByGenre(genre);
+    await promise.then(function(result) {
+        movies = [...result];
+    })
+
+    var promiseG = getGenres();
+    await promiseG.then(function(result) {
+        genres = [...result];
+    })
+
+
+    movies.forEach(async movie => {
+        genresInMovie = getGenresInMovie(movie, genres)
+
+        createMovie(movie, genresInMovie);
+        
+    }
+    );
+}
+
+function createMovie(movie, genresInMovie) {
+    const movieDiv = document.createElement('div');
+    movieDiv.classList.add('movie_box');
+    const myUrl = `http://image.tmdb.org/t/p/w185${movie.poster_path}`;
+    movieDiv.innerHTML = `
+        <div class = "cover "><img src="${myUrl}" style="width: 75%; height: 85%;"  alt="${movie.title}">
+            <p id= "styl2">${movie.title}</p>
+        </div>
+        <div class = "description ">
+            <div class = "description_header">
+                <h3>${movie.title}</h3>
+                <p>Premiere: ${movie.release_date}</p>
+            </div>
+            <div class = "description_body">
+                <h3>Movie description</h3>
+                <p id= "styl1">${movie.overview}</p>
+                <p id= "styl1" style="text-align:bottom">Vote: ${movie.vote_average} </p>
+            </div>
+            <div class = "description_bottom">
+                <h3>Genres</h3>
+                <p>${genresInMovie}</p>
+            </div>
+        </div>
+        `
+        ;
+    movieContainer.appendChild(movieDiv);
+}
---
+++ search_movie.js
@@ -0,0 +1,138 @@
+const f = document.getElementById('form');
+const input = document.getElementById('search-film');
+
+const movieContainerSearch = document.getElementById('movie-ctr');
+
+
+
+const optionsG = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/genre/movie/list?api_key=b9e8ab5199d674481be8e14eb992dc6a&language=en-US',
+};
+
+function getSearch(searchVal){
+    const options = {
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/search/movie?api_key=b9e8ab5199d674481be8e14eb992dc6a&query=${searchVal}`,
+        };
+    return axios.request(options).then(response => response.data.results)
+}
+
+function getGenres(){
+    return axios.request(optionsG).then(response => response.data.genres)
+}
+
+function getSearchMovie(search){
+    valueSearch = String();
+    for (const value of search) {
+        if(value != ' '){
+            valueSearch = valueSearch + value;
+        }
+        else{
+            valueSearch = valueSearch + '+' ;
+        }
+    };
+
+    return valueSearch;
+}
+
+function getGenresInMovie(movie, genres){
+    movieGenres = String();
+    var str = " ";
+    mGenres = Array();
+    mGenres = movie.genre_ids
+    mGenres.forEach(genre => {
+
+        genres.forEach(gen=> {
+        var g = gen.id;
+            if (genre == g){
+                movieGenres= movieGenres + str + gen.name;
+            };
+        });
+        
+    }
+    );
+    return movieGenres
+}
+
+function clearDiv(){
+    while (movieContainerSearch.firstChild) {
+        movieContainerSearch.removeChild(movieContainerSearch.firstChild);
+      }
+}
+
+async function loadSearch(event) {
+    movies = Array();
+    genres = Array();
+    const inputValue = input.value;
+    if (inputValue == ''){
+        return
+    }
+
+    var search = getSearchMovie(input.value);
+
+    var promise = getSearch(search);
+    await promise.then(function(result) {
+        movies = [...result];
+    })
+
+    var promiseG = getGenres();
+    await promiseG.then(function(result) {
+        genres = [...result];
+    })
+
+    if (Array.isArray(movies) && movies.length) {
+        clearDiv();
+
+        movies.forEach(movie => {
+            genresInMovie = getGenresInMovie(movie, genres)
+
+            createMovie(movie, genresInMovie);
+            
+        }
+        );
+    }
+    else{
+        createInfo();
+    }
+}
+function createInfo(){
+    const movieDiv = document.createElement('div');
+    movieDiv.classList.add('movie_box');
+    movieDiv.innerHTML = `
+    <h2>No result found for: ${input.value}</h2>
+    <p>Please try to enter something else </p>
+        `
+        ;
+    movieContainerSearch.insertBefore(movieDiv, movieContainerSearch.firstChild)
+}
+
+function createMovie(movie, genresInMovie) {
+    const movieDiv = document.createElement('div');
+    movieDiv.classList.add('movie_box');
+    const myUrl = `http://image.tmdb.org/t/p/w185${movie.poster_path}`;
+    movieDiv.innerHTML = `
+        <div class = "cover "><img src="${myUrl}" style="width: 85%; height: 85%;"  alt="${movie.title}">
+            <p id= "styl2">${movie.title}</p>
+        </div>
+        <div class = "description ">
+            <div class = "description_header">
+                <h3>${movie.title}</h3>
+                <p>Premiere: ${movie.release_date}</p>
+            </div>
+            <div class = "description_body">
+                <h3>Movie description</h3>
+                <p id= "styl1">${movie.overview}</p>
+                <p id= "styl1" style="text-align:bottom">Vote: ${movie.vote_average} </p>
+            </div>
+            <div class = "description_bottom">
+                <h3>Genres</h3>
+                <p>${genresInMovie}</p>
+            </div>
+        </div>
+        `
+        ;
+        movieContainerSearch.appendChild(movieDiv);
+}
+
+f.addEventListener('submit', loadSearch);
