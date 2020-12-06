a36e80de3cc8069dd666d07b6a8355ab63f6990c
Łukasz Popek <01123885@pw.edu.pl>
2020-12-06 16:02:29 +0100

    save data to file from form protected

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var registrButton = document.querySelectorAll('#formsStyle button')[0];
-var resetButton = document.querySelectorAll('#formsStyle button')[1];
-console.log(document.getElementById("#formsStyle"));
+
 
 
 function resetForm(){
@@ -13,7 +11,7 @@ function saveToTxt() {
   // Get the data from each element on the form.
   const name = document.getElementById('txtName');
   const surname = document.getElementById('txtSurname');
-  const birtday = document.getElementById('txtBirthday');
+  const birthday = document.getElementById('txtBirthday');
   const email = document.getElementById('txtEmail');
   const gender = document.getElementById('txtGender');
   
@@ -21,11 +19,19 @@ function saveToTxt() {
       '\r Name: ' + name.value + ' \r\n ' + 
       'Surname: ' + surname.value + ' \r\n ' + 
       'Email: ' + email.value + ' \r\n ' + 
-      'date of birthday: ' + birtday.value + ' \r\n ' + 
+      'date of birthday: ' + birthday.value + ' \r\n ' + 
       'Gender: ' + gender.value;
   
+  if (emailIsValid(email.value) == false)
+    {alert("Niepoprawny adress email!")};
+
+
+  if (name.value == '' || surname.value == '' || birthday == ''){
+    alert ("Nie wysyłaj niepełnego formularza")
+  }
+  else {
   const textToBLOB = new Blob([data], { type: 'text/plain' });
-  const sFileName = 'formData.txt';	   // The file to save the data.
+  const sFileName = 'formData.txt';
 
   let newLink = document.createElement("a");
   newLink.download = sFileName;
@@ -38,6 +44,11 @@ function saveToTxt() {
       newLink.style.display = "none";
       document.body.appendChild(newLink);
   }
-
   newLink.click(); 
+  }
+  
+}
+
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
